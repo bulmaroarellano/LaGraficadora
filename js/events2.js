4b3d61edb35c0961c742aa549746c87c1b13ff93
Bulmaro Arellano <tomaro@outlook.es>
2021-10-23 20:22:20 -0500

    Practice 2, bar charts corrected

--- js/events2.js
+++ js/events2.js
@@ -4,10 +4,25 @@ let pointX;
 let pointY;
 let prevX;
 let prevY;
-
+let errorValidacionInput=0;
 document.getElementById("clear").addEventListener("click", function () {
   location.reload();
 });
+
+
+function drawRec(pointX, pointY, i){
+  const colors = ['#fa4b2a','#e5989b', '#333d29', '#023047', '#5f0f40']
+  let px1, px2, py;
+  px1= pointX - 5; 
+  px2=pointX + 5;
+  py = pointY;
+  let width=px2-px1;
+console.log(px1 +" " + px2);
+  ctx.beginPath();
+  ctx.fillStyle=colors[i-1];
+  ctx.fillRect(px1, py,width, -py);
+return 1;  
+}
 
 function paintPoint2(e) {
   for (let i = 1; i < 6; i++) {
@@ -16,22 +31,22 @@ function paintPoint2(e) {
 
     if (parseFloat(pointX.value) && parseFloat(pointY.value)) {
       ctx.beginPath();
-      ctx.arc(pointX.value, pointY.value, 3, 0 * Math.PI, 2 * Math.PI);
-      ctx.fill();
-      ctx.stroke();
+      
 
       if (i == 1) {
         prevX = pointX.value;
-        prevY = pointY.value;
+        prevY = - pointY.value;
       } else if (i >= 2) {
         ctx.beginPath();
         ctx.moveTo(prevX, prevY);
         ctx.lineWidth = 1;
-        ctx.lineTo(pointX.value, pointY.value);
+        ctx.lineTo(pointX.value, -pointY.value);
         ctx.stroke();
         prevX = pointX.value;
-        prevY = pointY.value;
+        prevY = -pointY.value;
       }
+
+      drawRec(parseFloat(pointX.value),parseFloat(-pointY.value), i);
     } else {
       alert("Insert a valid number");
     }
